9879670aca2f96c1f4133e93fa8d21cbc7466171
mbahdashych <mbahdashych@cryptovoucher.io>
2022-01-08 20:26:47 +0100

    remove unnessasary imports

--- src/main/java/com/bankapp/bankwebapplication/repositories/PersonalClientRepository.java
+++ src/main/java/com/bankapp/bankwebapplication/repositories/PersonalClientRepository.java
@@ -1,7 +1,6 @@
 package com.bankapp.bankwebapplication.repositories;
 
 import com.bankapp.bankwebapplication.models.PersonClient;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
